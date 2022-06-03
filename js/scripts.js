572a9f7b7d8628fd29c62f477c7bc399d5cd23ad
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-03 12:01:10 -0700

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,26 @@ $(document).ready(function() {
     event.preventDefault();
     const beverage = parseInt($("#beverage").val());
     const food = parseInt($("#food").val());
-    const score = (beverage + food);
+    const animal = parseInt($("#animal").val());
+    const car = parseInt($("#car").val());
+    const score = (beverage + food + animal + car);
     if (food === 0 || beverage === 0 || animal === 0 || car === 0) {
       alert("Please complete all questions")
     }
-    else{
-          alert(score);
+    else if(score < 10) {
+      $("#output").text("programing language 1");
+      $("#empty").show();
+      $("#calculate").hide();
+    }
+    else if(score > 20) {
+      $("#output").text("programing language 2");
+      $("#empty").show();
+      $("#calculate").hide();
+    }
+    else if(score > 9) {
+      $("#output").text("programing language 3");
+      $("#empty").show();
+      $("#calculate").hide();
     }
   });
 
